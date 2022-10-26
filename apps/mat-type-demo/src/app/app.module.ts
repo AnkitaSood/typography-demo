d2c68bc9bfa8318b7a265eda04d027fbc0d5445c
ankita <asood@octolabs.io>
2022-10-26 18:19:15 -0500

    feat: new app with custom material typography

--- apps/mat-type-demo/src/app/app.module.ts
+++ apps/mat-type-demo/src/app/app.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonToggleModule} from "@angular/material/button-toggle";
+import {MatCardModule} from "@angular/material/card";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, MatButtonToggleModule, MatCardModule, MatButtonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
