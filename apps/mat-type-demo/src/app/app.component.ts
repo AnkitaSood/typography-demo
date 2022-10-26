77b14d13443b35e8086830edbb8663f5bd3ae6c3
ankita <asood@octolabs.io>
2022-10-26 16:34:17 -0500

    Initial commit

---
+++ apps/mat-type-demo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'typography-demo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'mat-type-demo';
+}
